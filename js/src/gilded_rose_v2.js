960d3ab4ef9aa92d64e8599284df6a48d9379207
Manokaran Kittuswamy <mkittuswamy@purestorage.com>
2022-05-20 07:38:09 +0530

    Prettified

--- js/src/gilded_rose_v2.js
+++ js/src/gilded_rose_v2.js
@@ -4,36 +4,40 @@ const MIN_QUALITY = 0;
 const MAX_QUALITY = 50;
 const NEGATIVE_MODIFIER = -1;
 const EXPIRY_MODIFIER = 2;
+const TO = "..";
 
 const ITEM_TYPES = {
-  'Sulfuras, Hand of Ragnaros': {
+  "Sulfuras, Hand of Ragnaros": {
     legendary: true,
   },
-  'Aged Brie': {
-    appreciate_on_aging: true
+  "Aged Brie": {
+    appreciate_on_aging: true,
   },
-  'Backstage passes to a TAFKAL80ETC concert': {
+  "Backstage passes to a TAFKAL80ETC concert": {
     appreciate_on_aging: true,
     expiry_quality: 0,
     quality_steps: {
-      '1..5': 3,
-      '6..10': 2,
-      '11..Infinity': 1
-    }
+      [`1${TO}5`]: 3,
+      [`6${TO}10`]: 2,
+      [`11${TO}Infinity`]: 1,
+    },
   },
-  'Conjured Mana Cake': {
+  "Conjured Mana Cake": {
     quality_steps: {
-      '-Infinity..Infinity': 2
-    }
-  }
+      [`-Infinity${TO}Infinity`]: 2,
+    },
+  },
+  "Misconfigured demon": {
+    quality_steps: {
+      [`Hell${TO}Highwater`]: 2,
+    },
+  },
 };
 
 var itemsCopy = [];
 
 function update_quality_v2() {
-
-  itemsCopy.forEach(item => {
-
+  itemsCopy.forEach((item) => {
     const itype = ITEM_TYPES[item.name] || {};
 
     if (itype.legendary) {
@@ -44,10 +48,18 @@ function update_quality_v2() {
     let quality_step = DEFAULT_QUALITY_STEP;
 
     if (itype.quality_steps) {
-      const sell_in_range = Object.keys(itype.quality_steps).find(sell_in_range => {
-        let [min, max] = sell_in_range.split('..').map(Number);
-        return min <= item.sell_in && item.sell_in <= max;
-      });
+      const sell_in_range = Object.keys(itype.quality_steps).find(
+        (sell_in_range) => {
+          let [min, max] = sell_in_range.split(TO).map(Number);
+          if (isNaN(min) || isNaN(max)) {
+            console.log(
+              `${sell_in_range} does not split neatly to numbers! Skipping this.`
+            );
+            return;
+          }
+          return min <= item.sell_in && item.sell_in <= max;
+        }
+      );
       if (sell_in_range) {
         quality_step = itype.quality_steps[sell_in_range];
       }
@@ -58,9 +70,9 @@ function update_quality_v2() {
     }
 
     if (item.sell_in <= 0 && itype.expiry_quality !== undefined) {
-      new_quality = itype.expiry_quality
-    } else if (item.sell_in <= 0) {  
-      new_quality = item.quality + (quality_step * EXPIRY_MODIFIER);
+      new_quality = itype.expiry_quality;
+    } else if (item.sell_in <= 0) {
+      new_quality = item.quality + quality_step * EXPIRY_MODIFIER;
     } else {
       new_quality = item.quality + quality_step;
     }
@@ -74,7 +86,5 @@ function update_quality_v2() {
 
     item.quality = new_quality;
     item.sell_in -= SELL_IN_STEP;
-
   });
-
 }
